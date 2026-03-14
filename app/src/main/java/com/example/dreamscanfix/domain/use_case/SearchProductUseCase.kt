c8e5000bf7c0859b453d5ee6ddacde6ccfc521dd
unknown <ron.garcia.santos@gmail.com>
2026-03-14 11:18:01 +0800

    feat(domain) expand use case to support OBJECT and MANUAL search

--- app/src/main/java/com/example/dreamscanfix/domain/use_case/SearchProductUseCase.kt
+++ app/src/main/java/com/example/dreamscanfix/domain/use_case/SearchProductUseCase.kt
@@ -1,14 +1,19 @@
 package com.example.dreamscanfix.domain.use_case
 
 import com.example.dreamscanfix.domain.model.Product
+import com.example.dreamscanfix.domain.model.SearchType
 import com.example.dreamscanfix.domain.repository.ProductRepository
 
 class SearchProductUseCase (
     private val repository: ProductRepository
 ) {
-    suspend operator fun invoke(barcode: String): Result<List<Product>> {
-        if (barcode.isBlank()) return Result.failure(Exception("Barcode is Empty"))
+    suspend operator fun invoke(query: String, type: SearchType): Result<List<Product>> {
+        if (query.isBlank()) return Result.failure(Exception("Search query cannot be Empty"))
 
-        return repository.searchProductByBarcode(barcode)
+        return when (type) {
+            SearchType.BARCODE -> repository.searchProductByBarcode(query)
+            SearchType.OBJECT -> repository.searchProductByObject(query)
+            SearchType.MANUAL -> repository.searchProductByManual(query)
+        }
     }
 }

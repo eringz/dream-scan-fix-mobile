f42e78948cc82537f5361842c45d74444b8a380d
unknown <ron.garcia.santos@gmail.com>
2026-03-16 21:52:23 +0800

    updated root to compatible plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    // Define the Compose Compiler plugin here
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.legacy.kapt) apply false
 }

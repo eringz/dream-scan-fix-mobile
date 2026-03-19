e9cdf0550c6b4b956bd50cc5b1ee1693bbfcfc60
unknown <ron.garcia.santos@gmail.com>
2026-03-19 21:57:31 +0800

    feat: read file for gemini api

--- app/src/main/java/com/example/dreamscanfix/MainActivity.kt
+++ app/src/main/java/com/example/dreamscanfix/MainActivity.kt
@@ -1,4 +1,4 @@
-    package com.example.dreamscanfix
+package com.example.dreamscanfix
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.dreamscanfix.ui.search.SearchScreen
 import com.example.dreamscanfix.ui.theme.DreamScanFixTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,4 +30,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-

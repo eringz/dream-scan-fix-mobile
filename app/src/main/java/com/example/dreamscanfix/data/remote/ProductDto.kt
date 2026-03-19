6c0bcc32dfda13c09e3d6ebac18074d4bda70d03
unknown <ron.garcia.santos@gmail.com>
2026-03-19 21:23:48 +0800

    feat(data): Adjusted the data type of price column

--- app/src/main/java/com/example/dreamscanfix/data/remote/ProductDto.kt
+++ app/src/main/java/com/example/dreamscanfix/data/remote/ProductDto.kt
@@ -2,7 +2,7 @@ package com.example.dreamscanfix.data.remote
 
 data class ProductDto (
     val title: String,
-    val price: Double,
+    val price: Double?,
     val imageUrl: String,
     val shopUrl: String
 )
